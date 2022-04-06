570595d7507c60dc36ba2d6f33053d1f8b628e76
ge34bec <an.nguyen@tum.de>
2022-04-06 07:46:22 +0200

    sas

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+// import {useEffect} from 'react'
 import Layout from '../components/Layout'
 import {AuthContextProvider} from '../context/authContext'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
@@ -6,12 +7,18 @@ import Head from 'next/head'
 import Script from 'next/head'
 
 function MyApp({ Component, pageProps }) {
+
+//   useEffect(() => {
+//     import("../node_modules/bootstrap/dist/js/bootstrap");
+// }, []);
+
   return <AuthContextProvider>
     <Head>
     <meta name='viewport' content ='width=device-width,initial-scale=1, maximum-scale=1, user-scalable=no' />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"  ></script>
 </Head>
-<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" />
-<Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous" />
+
+
     <Layout>
     <Component {...pageProps} />
   </Layout>
